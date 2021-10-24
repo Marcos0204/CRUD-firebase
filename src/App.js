100ec9ca0e4478f10d69f58b77cfbeba680c65f2
Marcos <marcosbastidas301@gmail.com>
2021-10-24 13:17:07 -0400

    auth-and-creating-user

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
 import Home from './components/Home';
 import Logueo from './components/Logueo';
+import firebaseApp from './Credentials';
+import {
+  getAuth,
+  onAuthStateChanged,
+} from 'firebase/auth';
+
+const auth = getAuth(firebaseApp);
 
 const App = () => {
 
   const [ userGlobal, setUserGlobal ] = useState(null);
+
+  //funcion que se ejecutar al detectar autentificacion
+  onAuthStateChanged(auth, (userAuth) =>{
+    if(userAuth){
+      setUserGlobal(userAuth);
+    } else {
+      setUserGlobal(null);
+    }
+  })
 
   return (
     <div>
--- src/components/Logueo.js
+++ src/components/Logueo.js
@@ -1,27 +1,42 @@
 import React, { useState } from "react";
 import { Stack, Container, Form, Button } from "react-bootstrap";
 
-// import firebaseApp from "../credenciales";
-// // import {
-// //   getAuth,
-// //   createUserWithEmailAndPassword,
-// //   signInWithEmailAndPassword,
-// //   signInWithRedirect,
-// //   GoogleAuthProvider,
-// // } from "firebase/auth";
-// // const auth = getAuth(firebaseApp);
-//const googleProvider = new GoogleAuthProvider();
+import firebaseApp from '../Credentials';
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+
+} from 'firebase/auth';
+
+const auth = getAuth(firebaseApp);
 
 const Logueo = () => {
  
     const [ iseRegister, SetIsRegister ] = useState(false)
+
+    async function submitHandler(e) {
+        e.preventDefault();
+        const email= e.target.formBasicEmail.value;
+        const password = e.target.formBasicPassword.value;
+        if(iseRegister){
+            const user =await createUserWithEmailAndPassword(auth, email, password);
+            console.log(user)
+        } else {
+            const user =await signInWithEmailAndPassword(auth, email, password);
+            console.log(user)
+        }
+        
+    }
 
   return (
     
     <Container>
         <Stack gap={3}>
             <h1>{iseRegister ? 'Rigistrate' : 'inicia sesion'}</h1>
-            <Form>
+            <Form 
+                onSubmit={submitHandler}
+            >
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" />
